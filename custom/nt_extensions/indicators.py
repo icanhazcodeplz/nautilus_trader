2b92d49a24a07b668154d47b174378ac26d3097a
icanhazcodeplz <67756774+icanhazcodeplz@users.noreply.github.com>
2025-12-21 12:04:57 -0800

    Change VWAPBands to use lower_scalar_multiplier and upper_scalar_multiplier, which are multipliers off of the 5% and 95% percentiles for lower and upper, respecivly

--- custom/nt_extensions/indicators.py
+++ custom/nt_extensions/indicators.py
@@ -1,7 +1,6 @@
 from collections import deque
 
 import numpy as np
-import pandas as pd
 
 from nautilus_trader.indicators import Indicator
 from nautilus_trader.model import TradeTick
@@ -9,27 +8,35 @@ from nautilus_trader.common.component import Logger
 
 
 class VWAPBands(Indicator):
-    initial_upper_lower_scalar = 0.50
-    adjust_every = 500
+    initial_upper_lower_scalar = 1.0
+    adjust_every = 100
 
     def __init__(
         self,
-        lower_pct: float,
-        upper_pct: float,
+        lower_scalar_multiplier: float,
+        upper_scalar_multiplier: float,
         rolling_window: int = 150,
-        variance_window_ratio: float = 1,
+        variance_window: int = 300,
         adjustment_window: int = 3000,
     ):
-        super().__init__(params=[lower_pct, upper_pct, rolling_window, variance_window_ratio, adjustment_window])
+        super().__init__(
+            params=[
+                lower_scalar_multiplier,
+                upper_scalar_multiplier,
+                rolling_window,
+                variance_window,
+                adjustment_window,
+            ]
+        )
         if rolling_window > adjustment_window:
             raise ValueError("Rolling window must be less than adjustment window")
         self.tick_lookback = adjustment_window
 
         self._log = Logger(name=self.__class__.__name__)
-        self.lower_pct = lower_pct
-        self.upper_pct = upper_pct
+        self.lower_scalar_multiplier = lower_scalar_multiplier
+        self.upper_scalar_multiplier = upper_scalar_multiplier
         self.rolling_window = rolling_window
-        self.variance_window_ratio = variance_window_ratio
+        self.variance_window = variance_window
         self.adjustment_window = adjustment_window
 
         self.lower_scalar = self.initial_upper_lower_scalar
@@ -38,21 +45,27 @@ class VWAPBands(Indicator):
         # Queues for the rolling window calculations
         self._trade_values = deque(maxlen=rolling_window)
         self._volumes = deque(maxlen=rolling_window)
-        # Make the variance window longer/shorter based on variance_window_ratio
-        self._variances = deque(maxlen=int(rolling_window * variance_window_ratio))
+        # Make the variance window longer/shorter based on variance_window
+        self._variances = deque(maxlen=variance_window)
 
         # Queues for the adjustment window calculations
         self._prices = deque(maxlen=adjustment_window)
-        self._values_for_adj = deque(maxlen=adjustment_window)
+        self._vwaps_for_adj = deque(maxlen=adjustment_window)
         self._mean_variances_for_adj = deque(maxlen=adjustment_window)
-
         self._window_filled = False
         self._adjust_counter = 1
+        self._lower_scalar_base = 0.0
+        self._upper_scalar_base = 0.0
 
-        self.value = None
+        self.vwap = None
         self.mean_variance = 0
+        self.lower_base = 0
         self.lower = 0
         self.upper = 0
+
+    @property
+    def value(self):
+        return self.vwap
 
     def handle_trade_tick(self, tick: TradeTick):
         self.update_raw(price=float(tick.price), volume=float(tick.size))
@@ -73,22 +86,28 @@ class VWAPBands(Indicator):
         if not self._window_filled:
             return
 
-        prices_array = np.array(self._prices)
-        values_arr = np.array(self._values_for_adj)
-        mean_variances_array = np.array(self._mean_variances_for_adj)
+        prices_arr = np.array(self._prices)
+        vwap_arr = np.array(self._vwaps_for_adj)
+        mean_variances_arr = np.array(self._mean_variances_for_adj)
 
-        upper_base = values_arr + mean_variances_array
-        diff_price_to_upper_base = prices_array - upper_base
-        upper_scalar = np.percentile(diff_price_to_upper_base, 100 - self.upper_pct)
+        # base values are average_var above or below vwap
+        lower_base = vwap_arr - mean_variances_arr
+        upper_base = vwap_arr + mean_variances_arr
 
-        lower_base = values_arr - mean_variances_array
-        diff_price_to_lower_base = prices_array - lower_base
-        lower_scalar = np.percentile(diff_price_to_lower_base, self.lower_pct)
-        lower_scalar = abs(lower_scalar)
+        # Get the differences from price to lower_base to get the base lower scalar. Find the 5th percentile to remove
+        # the bottom 5% outliers.
+        diff_price_to_lower_base = prices_arr - lower_base
+        self._lower_scalar_base = abs(np.percentile(diff_price_to_lower_base, 5))
 
-        self._log.info(f"Upper {self.upper_scalar}->{upper_scalar}, Lower {self.lower_scalar}->{lower_scalar}")
-        self.upper_scalar = upper_scalar
-        self.lower_scalar = lower_scalar
+        diff_price_to_upper_base = prices_arr - upper_base
+        self._upper_scalar_base = np.percentile(diff_price_to_upper_base, 95)
+
+        # The final lower scalar includes user defined multiplier
+        self.lower_scalar = self._lower_scalar_base * self.lower_scalar_multiplier
+        self.upper_scalar = self._upper_scalar_base * self.upper_scalar_multiplier
+
+        # self._log.info(f"Upper {self.upper_scalar}->{upper_scalar}, Lower {self.lower_scalar}->{lower_scalar}")
+        # self.lower_scalar = lower_scalar
 
     def update_raw(self, price, volume):
         # No weighting for this price (also avoiding divide by zero)
@@ -96,9 +115,9 @@ class VWAPBands(Indicator):
             return
 
         if not self.initialized:
-            if self.value is None:
+            if self.vwap is None:
                 # Need some initial value to start with to avoid raising
-                self.value = price
+                self.vwap = price
             self._set_has_inputs(True)  # What is this ever used for?
             # Wait to "set_intialized" until after window is fully filled.
             if self._window_filled:
@@ -110,15 +129,15 @@ class VWAPBands(Indicator):
         self._trade_values.append(price * volume)
         self._volumes.append(volume)
 
-        self.value = sum(self._trade_values) / sum(self._volumes)
+        self.vwap = sum(self._trade_values) / sum(self._volumes)
 
-        variance_from_val = abs(price - self.value)
+        variance_from_val = abs(price - self.vwap)
         self._variances.append(variance_from_val)
         self.mean_variance = sum(self._variances) / len(self._variances)
         self._mean_variances_for_adj.append(self.mean_variance)
 
         self._prices.append(price)
-        self._values_for_adj.append(self.value)
+        self._vwaps_for_adj.append(self.vwap)
 
         if self._adjust_counter % self.adjust_every == 0:
             self._adjust_counter = 1
@@ -126,17 +145,19 @@ class VWAPBands(Indicator):
         else:
             self._adjust_counter += 1
 
-        lower = self.lower_scalar if self._reached_max_window() else self.initial_upper_lower_scalar
-        upper = self.upper_scalar if self._reached_max_window() else self.initial_upper_lower_scalar
+        lower_scalar = self.lower_scalar if self._reached_max_window() else self.initial_upper_lower_scalar
+        upper_scalar = self.upper_scalar if self._reached_max_window() else self.initial_upper_lower_scalar
 
-        self.lower = self.value - self.mean_variance - lower
-        self.upper = self.value + self.mean_variance + upper
+        self.lower_base = self.vwap - self.mean_variance
+        self.lower = self.lower_base - lower_scalar
+
+        self.upper = self.vwap + self.mean_variance + upper_scalar
 
     def _reset(self):
         raise NotImplementedError
         self._trade_values = deque(maxlen=self.rolling_window)
         self._volumes = deque(maxlen=self.rolling_window)
-        self.value = 0
+        self.vwap = 0
 
 
 class RollingTimeVWAP(Indicator):
