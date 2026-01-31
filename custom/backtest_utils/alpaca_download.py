ef57bd4e28085bd1957543acb5ad06b23b991cc7
icanhazcodeplz <67756774+icanhazcodeplz@users.noreply.github.com>
2026-01-31 11:14:19 -0500

    improve efficiency and automate stocks to get using top_gainers from trading_dev repo

--- custom/backtest_utils/alpaca_download.py
+++ custom/backtest_utils/alpaca_download.py
@@ -1,8 +1,11 @@
+import json
+
 import pandas as pd
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockTradesRequest, StockQuotesRequest
 
 from custom.backtest_utils.load_catalog_data import BACKTESTING_CATALOG
+from custom.catalog_options import write_json_single_line_entries
 from nautilus_trader.adapters.alpaca import ALPACA
 from nautilus_trader.adapters.alpaca.utils import get_alpaca_key_and_secret
 from nautilus_trader.core.datetime import dt_to_unix_nanos
@@ -12,6 +15,7 @@ from nautilus_trader.model.enums import AggressorSide
 from nautilus_trader.model.identifiers import TradeId
 from nautilus_trader.model.objects import Price, Quantity
 from nautilus_trader.test_kit.providers import TestInstrumentProvider
+
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
@@ -48,6 +52,7 @@ client = StockHistoricalDataClient(api_key, api_secret, raw_data=False)
 
 
 def get_trades_and_save_to_catalog_if_needed(symbol, start_dt_str, end_dt_str, force=False):
+    """Download trades from Alpaca and save to catalog."""
     instrument_id = TestInstrumentProvider.equity(symbol=symbol, venue=ALPACA).id
 
     # Check if trades exist for the range in BACKTESTING_CATALOG
@@ -59,9 +64,7 @@ def get_trades_and_save_to_catalog_if_needed(symbol, start_dt_str, end_dt_str, f
             end=end_dt_str,
         )
         if existing_trades:
-            print(
-                f"Trades already exist for {symbol} from {start_dt_str} to {end_dt_str}, skipping download"
-            )
+            print(f"Trades exist for {symbol} from {start_dt_str} to {end_dt_str}, skipping")
             return
 
     request = StockTradesRequest(
@@ -69,139 +72,130 @@ def get_trades_and_save_to_catalog_if_needed(symbol, start_dt_str, end_dt_str, f
     )
 
     # Fetch the tick data
-    trades = client.get_stock_trades(request)
-    df = trades.df  # TODO: Converting to df is slow. Could do without maybe?
-    df = df.reset_index()
-    print(f"Fetched {len(df)} trades")
-    df = df[df["exchange"] != "D"]
-    print(f"Droped Finra. Now {len(df)} trades")
+    alpaca_response = client.get_stock_trades(request)
+    trades = alpaca_response.data[symbol]
 
-    # Convert each row in df into a TradeTick object and then save to the catalog `BACKTESTING_CATALOG`
-    trade_ticks = []
-    for i, row in df.iterrows():  # TODO: Do in a faster way?
-        ts_event = dt_to_unix_nanos(row["timestamp"])
-        ts_init = ts_event  # Use same timestamp for initialization
+    any_penny = any(trade.price < 1 for trade in trades)
+    if any_penny:
+        raise ValueError(f"Penny stocks not yet supported")
 
-        size_ = int(row["size"])
-        if size_ == 0:
-            continue
-        # price_from_str = Price.from_str(str(row["price"]))
-
-        precision = 2
-        price_float = float(row["price"])
-        if price_float < 1:
-            raise ValueError(f"Penny stocks not yet supported")
-        trade_tick = TradeTick(
+    trade_ticks = [
+        TradeTick(
             instrument_id=instrument_id,
-            price=Price(price_float, precision=precision),
-            size=Quantity.from_str(str(size_)),
-            aggressor_side=AggressorSide.NO_AGGRESSOR,  # Alpaca doesn't provide aggressor side
+            price=Price(trade.price, precision=2),
+            size=Quantity.from_int(int(trade.size)),
+            aggressor_side=AggressorSide.NO_AGGRESSOR,
             trade_id=TradeId(str(i)),
-            ts_event=ts_event,
-            ts_init=ts_init,
+            ts_event=(ts := dt_to_unix_nanos(trade.timestamp)),  # Look at me! I'm a WAAAAAAALLLrus
+            ts_init=ts,
         )
-        trade_ticks.append(trade_tick)
+        for i, trade in enumerate(trades)
+        if trade.exchange != "D" and trade.size != 0
+    ]
 
     BACKTESTING_CATALOG.write_data(trade_ticks)
 
 
-def get_quotes_and_save_to_catalog(symbol, start_dt_str, end_dt_str):
+def get_quotes_and_save_to_catalog(symbol, start_dt_str, end_dt_str, force=False):
     """Download quote data from Alpaca and save as QuoteTicks to catalog."""
+    instrument_id = TestInstrumentProvider.equity(symbol=symbol, venue=ALPACA).id
+
+    # Check if quotes exist for the range in BACKTESTING_CATALOG
+    if not force:
+        existing_quotes = BACKTESTING_CATALOG.query(
+            data_cls=QuoteTick,
+            identifiers=[str(instrument_id)],
+            start=start_dt_str,
+            end=end_dt_str,
+        )
+        if existing_quotes:
+            print(f"Quotes exist for {symbol} from {start_dt_str} to {end_dt_str}, skipping")
+            return
+
     request = StockQuotesRequest(
-        feed="sip",
-        symbol_or_symbols=symbol,
-        start=start_dt_str,
-        end=end_dt_str,
-        limit=None,
+        feed="sip", symbol_or_symbols=symbol, start=start_dt_str, end=end_dt_str
     )
 
-    instrument_id = TestInstrumentProvider.equity(symbol=symbol, venue=ALPACA).id
-    start_at = pd.Timestamp.now()
-    quotes = client.get_stock_quotes(request)
-    print(f"Downloading took {(pd.Timestamp.now() - start_at).total_seconds()} seconds")
-    df = quotes.df
-    print(f"Fetched {len(df)} quotes")
-    df = df.reset_index()
+    alpaca_response = client.get_stock_quotes(request)
+    quotes = alpaca_response.data[symbol]
 
-    # Convert each row in df into a QuoteTick object and then save to the catalog
-    start_at = pd.Timestamp.now()
-    quote_ticks = []
-    for _, row in df.iterrows():
-        # Convert timestamp to Unix nanoseconds
-        ts_event = dt_to_unix_nanos(row["timestamp"])
-        ts_init = ts_event  # Use same timestamp for initialization
-
-        # Skip quotes with zero bid or ask sizes
-        bid_size = int(row["bid_size"])
-        ask_size = int(row["ask_size"])
-        # if bid_size == 0 or ask_size == 0:
-        #     continue
-
-        # Create QuoteTick
-        bid_price = row["bid_price"]
-        ask_price = row["ask_price"]
-        bid_price_Price = Price.from_str(str(bid_price))
-        ask_price_Price = Price.from_str(str(ask_price))
-        precision = max(ask_price_Price.precision, bid_price_Price.precision, 2)
-        # if precision > 2:
-        #     print(f"WARNING: precision {precision} is greater than 2, setting to 2")
-        precision = 2
-        quote_tick = QuoteTick(
+    quote_ticks = [
+        QuoteTick(
             instrument_id=instrument_id,
-            bid_price=Price(float(bid_price), precision=precision),
-            ask_price=Price(float(ask_price), precision=precision),
-            bid_size=Quantity.from_str(str(bid_size)),
-            ask_size=Quantity.from_str(str(ask_size)),
-            ts_event=ts_event,
-            ts_init=ts_init,
+            bid_price=Price(quote.bid_price, precision=2),
+            ask_price=Price(quote.ask_price, precision=2),
+            bid_size=Quantity.from_int(int(quote.bid_size)),
+            ask_size=Quantity.from_int(int(quote.ask_size)),
+            ts_event=(ts := dt_to_unix_nanos(quote.timestamp)),
+            ts_init=ts,
         )
-        quote_ticks.append(quote_tick)
-    print(f"Conversion took {(pd.Timestamp.now() - start_at).total_seconds()} seconds")
-    print("Saving to catalog")
-    # Write to catalog
-    start_at = pd.Timestamp.now()
+        for quote in quotes
+    ]
+
     BACKTESTING_CATALOG.write_data(quote_ticks)
-    print(f"Writing to Catalog took {(pd.Timestamp.now() - start_at).total_seconds()} seconds")
 
 
-def prepare_alpaca_data(symbol, start_dt_str):
-    end_dt_str = pd.Timestamp(start_dt_str) + pd.Timedelta(days=1)
-    end_dt_str = end_dt_str.strftime("%Y-%m-%d")
-    print(f"Getting Alpaca data for {symbol} from {start_dt_str} to {end_dt_str}")
-    get_trades_and_save_to_catalog(symbol, start_dt_str, end_dt_str)
+def prepare_alpaca_data(symbol, start_dt):
+    end_dt = start_dt.replace(hour=0, minute=0) + pd.Timedelta(days=1)
+    start_dt_str = start_dt.isoformat()
+    end_dt_str = end_dt.isoformat()
+    get_trades_and_save_to_catalog_if_needed(symbol, start_dt_str, end_dt_str)
     get_quotes_and_save_to_catalog(symbol, start_dt_str, end_dt_str)
 
 
 if __name__ == "__main__":
+    import os
+    from pathlib import Path
 
-       9/19 - ZOOZ
-       9/19 - AGMH
-       
-       pulled already
-       10/13 - CHNR
-       11/13 - SGBX
-       10/14 - JDZG
-       10/14 - GWAV
-       10/14 - NVA
-       12/01 - CNCK
-       12/15 - RADX
-       12/16 - AMCI
-       
-       12/11 - BTTC
-       12/11 - CRBP
-       12/11 - MIGI
-       12/11 - AXIL
-       12/12 - JZXN
-       12/19 - AZI
-       12/17 - MBRX
-       12/18 - LHAI
-              
-       """
+    top_gainers_dir = "/Users/brent/code/trading_dev/data/top_gainers"
+    parquet_files = sorted([f for f in os.listdir(top_gainers_dir) if f.endswith(".parquet")])[-4:]
 
-    symbol = "pavs".upper()
-    start_dt_str = "2025-12-18"
+    price_min = 2.0
+    price_max = 20.0
+    vol_min = 100_000
+    check_hours = [5, 7, 10, 13, 17]
+    must_be_in_top = 5
+    min_perc_gain = 40
 
-    prepare_alpaca_data(symbol, start_dt_str)
+    for parquet_file in parquet_files:
+        top_gainers_df = pd.read_parquet(Path(top_gainers_dir) / parquet_file)
+        print(parquet_file)
+        for hr in check_hours:
+            start_dt = top_gainers_df.timestamp[0].replace(hour=hr, minute=0)
+            candidates = top_gainers_df[top_gainers_df["timestamp"] == start_dt].head(
+                must_be_in_top
+            )
+            candidates = candidates[
+                (candidates["price"] > price_min)
+                & (candidates["price"] < price_max)
+                & (candidates["volume"] > vol_min)
+                & (candidates["perc_gain"] > min_perc_gain)
+            ]
+            print(f"{candidates}\n")
+            for symbol in candidates["symbol"].unique():
+                data_start_dt = start_dt - pd.Timedelta(hours=1)
+                prepare_alpaca_data(symbol, data_start_dt)
+
+                # Add entry to catalog_options.json
+                catalog_json_path = "/Users/brent/code/nautilus_trader/custom/catalog_options.json"
+                with open(catalog_json_path) as f:
+                    catalog_options = json.load(f)
+
+                data_end_dt = data_start_dt.replace(hour=0, minute=0) + pd.Timedelta(days=1)
+                date_str = data_start_dt.strftime("%m%d")
+                key = f"{date_str}_{symbol.lower()}"
+                # Check if the key is already
+                if key in catalog_options.keys():
+                    print(f"{key} already in catalog_options. Skipping.")
+
+                catalog_options[key] = {
+                    "symbol": symbol.upper(),
+                    "start": data_start_dt.strftime("%Y-%m-%d %H:%M"),
+                    "end": data_end_dt.strftime("%Y-%m-%d %H:%M"),
+                    "notes": "",
+                }
+
+                write_json_single_line_entries(catalog_options, catalog_json_path)
 
     if False:
         end_dt_str = pd.Timestamp(start_dt_str) + pd.Timedelta(days=1)
@@ -250,10 +244,18 @@ if __name__ == "__main__":
         data_cls = TradeTick
         # data_cls = QuoteTick
         a_data = get_catalog_data(
-            symbol, f"{start_dt_str}T00:00:00Z", f"{end_dt_str}T00:00:00Z", data_cls=data_cls, venue=ALPACA
+            symbol,
+            f"{start_dt_str}T00:00:00Z",
+            f"{end_dt_str}T00:00:00Z",
+            data_cls=data_cls,
+            venue=ALPACA,
         )
         b_data = get_catalog_data(
-            symbol, f"{start_dt_str}T00:00:00Z", f"{end_dt_str}T00:00:00Z", data_cls=data_cls, venue=DATABENTO
+            symbol,
+            f"{start_dt_str}T00:00:00Z",
+            f"{end_dt_str}T00:00:00Z",
+            data_cls=data_cls,
+            venue=DATABENTO,
         )
 
         if data_cls == TradeTick:
