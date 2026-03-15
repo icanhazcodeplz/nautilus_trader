b10f8d9a6cb27904107d6d7a8141811d2b14ceb2
icanhazcodeplz <67756774+icanhazcodeplz@users.noreply.github.com>
2026-03-15 10:18:37 -0400

    updates

--- custom/catalog_options.py
+++ custom/catalog_options.py
@@ -17,3 +17,12 @@ def write_json_single_line_entries(data: dict, path: str):
 _json_path = Path(__file__).parent / "catalog_options.json"
 with open(_json_path) as f:
     CATALOG_OPTIONS = json.load(f)
+
+
+def extract_dataset_name_info(dataset_name):
+    catalog_params = CATALOG_OPTIONS[dataset_name.lower()]
+    symbol = catalog_params["symbol"].upper()
+    start_str = catalog_params["start"]
+    end_str = catalog_params["end"]
+    return symbol, start_str, end_str
+
