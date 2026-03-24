b9223a9bad7241f920b243603b7899c4ec4deed1
icanhazcodeplz <67756774+icanhazcodeplz@users.noreply.github.com>
2026-03-24 13:33:56 -0400

    add support for penny stocks

--- custom/strategies/_tiers.py
+++ custom/strategies/_tiers.py
@@ -2,22 +2,24 @@ from copy import copy
 
 import math
 
+from custom.utils.precision_utils import make_Price
+
 
 def market_round_up(price: float) -> float:
     if price < 1.0:
-        raise NotImplementedError("Market rounding not implemented for penny stocks")
+        return math.ceil(price * 10000) / 10000
     return math.ceil(price * 100) / 100
 
 
 class Tiers:
-    def __init__(self, quantity, starting_price, mean_variance, num_tiers, instrument):
+    def __init__(self, quantity, starting_price, mean_variance, num_tiers):
         self.quantity = quantity
         if quantity < num_tiers:
             num_tiers = quantity
 
         step_size = self._get_step_size(starting_price, mean_variance)
         target_prices = self._get_tier_prices(num_tiers, starting_price, step_size)
-        target_prices = [instrument.make_price(price) for price in target_prices]
+        target_prices = [make_Price(price) for price in target_prices]
         target_qtys = self._get_tier_quantities(num_tiers, quantity)
         self.max_qty_per_tier = max(target_qtys)
         self.prices = set(target_prices)
@@ -27,7 +29,7 @@ class Tiers:
     def _get_step_size(price: float, mean_variance: float) -> float:
         # TODO: Make this more intelligent
         if price < 1.0:
-            raise NotImplementedError("Step size not implemented for penny stocks")
+            return 0.005
         if mean_variance > 0.10:
             return 0.02
         return 0.01
@@ -37,15 +39,20 @@ class Tiers:
         lowest_tier_price = market_round_up(low_price)
         if tier_count == 1:
             return [lowest_tier_price]
-        dollars = int(lowest_tier_price)
-        cents = int((lowest_tier_price - dollars) * 100)
-        step_cents = int(step_size * 100)
-
-        # Adjust cents to be the next integer up that is evenly divided by step_cents
-        if cents % step_cents != 0:
-            cents = ((cents // step_cents) + 1) * step_cents
-
-        lowest_tier_price = dollars + (cents / 100)
+        if step_size < 0.01:
+            ticks = int(round(lowest_tier_price * 10000))
+            step_ticks = int(round(step_size * 10000))
+            if ticks % step_ticks != 0:
+                ticks = ((ticks // step_ticks) + 1) * step_ticks
+            lowest_tier_price = ticks / 10000
+        else:
+            dollars = int(lowest_tier_price)
+            cents = int(round((lowest_tier_price - dollars) * 100, 0))
+            step_cents = int(step_size * 100)
+            if cents % step_cents != 0:
+                cents = ((cents // step_cents) + 1) * step_cents
+            lowest_tier_price = dollars + (cents / 100)
+        # Ensure tier price never drops below the starting price
         return [lowest_tier_price + (i * step_size) for i in range(tier_count)]
 
     @staticmethod
