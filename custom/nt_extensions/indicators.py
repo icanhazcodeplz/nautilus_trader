f0c51b5e9a516447bcee6963fd7a52d5d32e6c31
icanhazcodeplz <67756774+icanhazcodeplz@users.noreply.github.com>
2026-09-16 10:26:15 -0400

    add RollingVWAP

--- custom/nt_extensions/indicators.py
+++ custom/nt_extensions/indicators.py
@@ -389,3 +389,38 @@ class RollingTimeVWAP(Indicator):
 
     def _reset(self):
         raise Exception("Not supported")
+
+
+class RollingVWAP(Indicator):
+    """
+    VWAP over the last `rolling_window` trades.
+    """
+
+    def __init__(self, rolling_window: int):
+        super().__init__(params=[rolling_window])
+
+        self.rolling_window = rolling_window
+
+        self._trade_values = deque(maxlen=rolling_window)
+        self._volumes = deque(maxlen=rolling_window)
+        self.value = None
+
+    def handle_trade_tick(self, tick: TradeTick):
+        self.update_raw(price=float(tick.price), volume=float(tick.size))
+
+    def update_raw(self, price, volume):
+        # No weighting for this price (also avoiding divide by zero)
+        if volume == 0:
+            return
+
+        if not self.initialized:
+            self._set_has_inputs(True)
+            self._set_initialized(True)
+
+        self._trade_values.append(price * volume)
+        self._volumes.append(volume)
+
+        self.value = sum(self._trade_values) / sum(self._volumes)
+
+    def _reset(self):
+        raise Exception("Not supported")
